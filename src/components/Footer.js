9bb31868419ad574f9eeb68c0539c667f780c42d
BenjaminOyarzun17 <53401911+BenjaminOyarzun17@users.noreply.github.com>
2021-05-06 22:11:49 -0400

    footerFix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,10 +12,10 @@ class Footer extends React.Component{
        
        
        <Row>
-          <Col><p style={{margin:"10px"}}><a href="kschreiner@dsmorus.cl">Contacto </a></p></Col>
+          <Col><p style={{margin:"10px"}}>Contacto: kschreiner@dsmorus.cl </p></Col>
           <Col><p style={{margin:"10px"}}><a href="https://discord.gg/tPpUskwuD5">Discord </a></p></Col>
             <Col><p style={{margin:"10px"}}><a href="/">Instagram </a></p></Col>
-            <Col><p style={{margin:"10px"}}><a href="/">Whatsapp</a></p></Col>
+            <Col><p style={{margin:"10px"}}>Whatsapp:</p></Col>
         </Row>
       </footer>
        
