178ef58dc3df72e54589e4db47ea3ac35fcf26f3
BenjaminOyarzun17 <53401911+BenjaminOyarzun17@users.noreply.github.com>
2021-05-09 16:02:31 -0400

    analytics

--- src/images/generadorQr.py
+++ src/images/generadorQr.py
@@ -1,10 +1,12 @@
 import qrcode
 
 qr = qrcode.QRCode(version =1,error_correction=qrcode.constants.ERROR_CORRECT_L,box_size=50, border =2)
-qr.add_data("https://caamorus2021.com/#/")
+#qr.add_data("https://caamorus2021.com/#/")
+qr.add_data("https://www.instagram.com/soywapolistaa/")
 
 qr.make (fit = True)
 
 img = qr.make_image(fill_color = "black", back_color  = "white")
 
-img.save("listaAQR.png")
+#img.save("listaAQR.png")
+img.save("instagramListaAQR.png")
